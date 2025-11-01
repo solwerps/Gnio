eab8cbcfeff05355096ec4be2fde8e7b031322d6
solwerps@gmail.com <158217681+solwerps@users.noreply.github.com>
2025-11-01 08:52:57 -0600

    version 1

--- src/app/dashboard/contador/[usuario]/empresas/[id]/page.tsx
+++ src/app/dashboard/contador/[usuario]/empresas/[id]/page.tsx
@@ -16,10 +16,11 @@ export default function EmpresaDashboardPage() {
       <EmpresaSidebar empresaId={empresaId} forceUsuario={String(usuario)} />
 
       <main className="flex-1 p-8">
-        <h1 className="text-4xl font-bold">Dashboard</h1>
+        <h1 className="text-4xl font-bold">Dashboard/listo para la experiencia</h1>
         <p className="mt-6 text-lg">
           HOLA “{params.id}” Soy <span className="font-bold">GNIO</span> Bienvenido a tu entorno contable
         </p>
+
       </main>
     </div>
   );
